2a57e897893146115b2cfe85e539b6745bf0ff48
angcyo <angcyo@126.com>
2016-10-18 17:14:20 +0800

    星期二 2016-10-18

--- app/src/main/java/com/angcyo/keyboarddemo/MainActivity.java
+++ app/src/main/java/com/angcyo/keyboarddemo/MainActivity.java
@@ -50,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
                 logHit(decorView);
 
                 logVisible(mContentView);
-                logVisible(decorView);//test
+                logVisible(decorView);//test test
             }
         });
 
